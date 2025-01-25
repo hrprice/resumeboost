5bd1797b29d985962268e264ce81440ea5287d12
Henry Price <halrprice@gmail.com>
2025-01-24 20:36:31 -0500

    bundle splitting, unused deps

--- packages/ui/src/Routing.tsx
+++ packages/ui/src/Routing.tsx
@@ -1,16 +1,27 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, Suspense, lazy, useEffect } from "react";
 import { useAuthContext } from "@resume-optimizer/ui/state/use-auth-context";
 import Text from "@resume-optimizer/ui/components/Text";
-import Login from "@resume-optimizer/ui/pages/auth/Login";
-import HomePage from "@resume-optimizer/ui/pages/home/HomePage";
-import ChatPage from "@resume-optimizer/ui/pages/chat/ChatPage";
-import Signup from "@resume-optimizer/ui/pages/auth/Signup";
-import JobsPage from "@resume-optimizer/ui/pages/jobs/JobsPage";
-import JobDetailsPage from "@resume-optimizer/ui/pages/jobs/JobDetailsPage";
 import { useBreakpoint } from "./state/use-breakpoint";
-import MobileComingSoonPage from "./pages/MobileComingSoonPage";
-import ResumePage from "./pages/resume/ResumePage";
+
+// Lazy imports for the components
+const Login = lazy(() => import("@resume-optimizer/ui/pages/auth/Login"));
+const Signup = lazy(() => import("@resume-optimizer/ui/pages/auth/Signup"));
+const HomePage = lazy(() => import("@resume-optimizer/ui/pages/home/HomePage"));
+const ChatPage = lazy(() => import("@resume-optimizer/ui/pages/chat/ChatPage"));
+const JobsPage = lazy(() => import("@resume-optimizer/ui/pages/jobs/JobsPage"));
+const JobDetailsPage = lazy(
+  () => import("@resume-optimizer/ui/pages/jobs/JobDetailsPage")
+);
+const MobileComingSoonPage = lazy(
+  () => import("@resume-optimizer/ui/pages/MobileComingSoonPage")
+);
+const ResumePage = lazy(
+  () => import("@resume-optimizer/ui/pages/resume/ResumePage")
+);
+const CircularProgressBox = lazy(
+  () => import("@resume-optimizer/ui/components/CircularProgressBox")
+);
 
 const AuthenticatedRoute = ({ children }: { children: ReactNode }) => {
   const { user } = useAuthContext();
@@ -31,70 +42,72 @@ const Routing = () => {
   if (isMobile) return <MobileComingSoonPage />;
 
   return (
-    <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route
-        path="/login"
-        element={
-          <AuthenticatedRoute>
-            <Login />
-          </AuthenticatedRoute>
-        }
-      />
-      <Route
-        path="/chat"
-        element={
-          <AuthenticatedRoute>
-            <ChatPage />
-          </AuthenticatedRoute>
-        }
-      />
-      <Route
-        path="/jobs"
-        element={
-          <AuthenticatedRoute>
-            <JobsPage />
-          </AuthenticatedRoute>
-        }
-      />
-      <Route
-        path="/jobs/:conversationId"
-        element={
-          <AuthenticatedRoute>
-            <JobDetailsPage />
-          </AuthenticatedRoute>
-        }
-      />
-      <Route
-        path="/resume"
-        element={
-          <AuthenticatedRoute>
-            <ResumePage />
-          </AuthenticatedRoute>
-        }
-      />
-      {/* <Route path="/verify-email" element={<VerifyEmail />} /> */}
-      <Route
-        path="/"
-        element={
-          <AuthenticatedRoute>
-            <HomePage />
-          </AuthenticatedRoute>
-        }
-      />
-      <Route
-        path="*"
-        element={
-          <div className="flex justify-center bg-surface">
-            <div className="bg-background container h-full flex items-center justify-center">
-              <Text variant="h4" className="text-primary-default">
-                Page not found
-              </Text>
+    <Suspense fallback={<CircularProgressBox />}>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/login"
+          element={
+            <AuthenticatedRoute>
+              <Login />
+            </AuthenticatedRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <AuthenticatedRoute>
+              <ChatPage />
+            </AuthenticatedRoute>
+          }
+        />
+        <Route
+          path="/jobs"
+          element={
+            <AuthenticatedRoute>
+              <JobsPage />
+            </AuthenticatedRoute>
+          }
+        />
+        <Route
+          path="/jobs/:conversationId"
+          element={
+            <AuthenticatedRoute>
+              <JobDetailsPage />
+            </AuthenticatedRoute>
+          }
+        />
+        <Route
+          path="/resume"
+          element={
+            <AuthenticatedRoute>
+              <ResumePage />
+            </AuthenticatedRoute>
+          }
+        />
+        {/* <Route path="/verify-email" element={<VerifyEmail />} /> */}
+        <Route
+          path="/"
+          element={
+            <AuthenticatedRoute>
+              <HomePage />
+            </AuthenticatedRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <div className="flex justify-center bg-surface">
+              <div className="bg-background container h-full flex items-center justify-center">
+                <Text variant="h4" className="text-primary-default">
+                  Page not found
+                </Text>
+              </div>
             </div>
-          </div>
-        }
-      />
-    </Routes>
+          }
+        />
+      </Routes>
+    </Suspense>
   );
 };
 export default Routing;
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import withMT from "@material-tailwind/react/utils/withMT";
-export default withMT({
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -65,4 +64,4 @@ export default withMT({
     },
   },
   plugins: [],
-});
+};
